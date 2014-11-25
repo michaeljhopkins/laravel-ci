2b1953a0f1621e479c7fa08ba112830e3e1472ff
Antonio Carlos Ribeiro <acr@antoniocarlosribeiro.com>
2014-11-25 20:24:44 -0200

    Upgraded

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -6,6 +6,10 @@ class HomeController extends Controller {
 
 	public function index()
 	{
+//		$data = \App::make('PragmaRX\Ci\Data\Repositories\Data');
+//
+//		$data->storeTestResult(\PragmaRX\Ci\Vendor\Laravel\Entities\Test::find(13), 'ERRRRRRRRR', false);
+//
 		return view('pragmarx/ci::dashboard');
 	}
 
